f123c71a3a74dec092ab44338766e4d675b46734
Noah Applegate <noahapplegate1@gmail.com>
2021-11-09 22:01:35 -0800

    feature change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello.
+          Changed this in feature.
         </p>
         <a
           className="App-link"
